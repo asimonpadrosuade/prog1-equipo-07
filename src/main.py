bc2676c520f472bde91efbf6be439ebac9b58593
Agustin Simon Padros <asimonpadros@uade.edu.ar>
2025-09-12 11:38:12 -0300

    Agregar ventana inicial y lógica de precios de películas

---
+++ src/main.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import tkinter
+
+ventana = tkinter.Tk()
+ventana.geometry("1080x720")
+
+titulo = tkinter.Label(ventana, text = "Bienvenido a PyTicket")
+titulo.pack()
+
+ventana.mainloop()
+
+peliculas = {
+    1: {"titulo": "El Conjuro 4", "fecha_lanzamiento": datetime(2025, 9, 5),  "categoría": "Terror", "idioma": "Subtitulada", "horarios": ["15:30", "18:00", "20:45"]},
+    2: {"titulo": "Superman", "fecha_lanzamiento": datetime(2025, 7, 10), "categoría": "Accion", "idioma": "Castellano", "horarios": ["14:15", "17:00", "21:10"]},
+    3: {"titulo": "Tron: Ares", "fecha_lanzamiento": datetime(2025, 10, 10), "categoría": "Accion", "idioma": "Subtitulada", "horarios": ["16:00", "19:30", "22:15"]},
+    4: {"titulo": "Homo Argentum", "fecha_lanzamiento": datetime(2025, 8, 14), "categoría": "Comedia", "idioma": "Castellano", "horarios": ["13:50", "18:40", "20:10"]},
+    5: {"titulo": "Mascotas al Rescate", "fecha_lanzamiento": datetime(2025, 8, 25), "categoría": "Animacion", "idioma": "Castellano", "horarios": ["12:30", "16:45", "19:00"]},
+}
+
+def formatear_moneda(valor):
+    entero, dec = f"{valor:,.2f}".split(".")
+    return f"$ {entero.replace(',', '.')},{dec}"
+
+def precio_pelicula(fecha_lanzamiento):
+  precio_estandar = 7999 #precio estandar
+  precio_estreno = precio_estandar * 1.35 #35% mas caro por estreno
+  hoy = datetime.now()
+  dias_estreno = (hoy - fecha_lanzamiento).days
+  if dias_estreno<7:
+    return precio_estreno
+  else:
+    return precio_estandar
