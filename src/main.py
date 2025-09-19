6eaaf01c248000c2e4991cf11f5583e74dd79478
Your Name <you@example.com>
2025-09-18 23:43:01 -0300

    precios de las entrdas

--- src/main.py
+++ src/main.py
@@ -21,12 +21,16 @@ def formatear_moneda(valor):
     entero, dec = f"{valor:,.2f}".split(".")
     return f"$ {entero.replace(',', '.')},{dec}"
 
-def precio_pelicula(fecha_lanzamiento):
-  precio_estandar = 7999 #precio estandar
-  precio_estreno = precio_estandar * 1.35 #35% mas caro por estreno
-  hoy = datetime.now()
-  dias_estreno = (hoy - fecha_lanzamiento).days
-  if dias_estreno<7:
-    return precio_estreno
-  else:
-    return precio_estandar
+def precio_pelicula(edad, movistar):
+    precio_estandar = 7999   
+    precio_movistar = 7500   
+    precio_reducido = 6999   
+    if edad > 65:
+        return "Jubilado", formatear_moneda(precio_reducido)
+    elif edad < 6:
+        return "Menor", formatear_moneda(precio_reducido)
+    elif movistar == 1:
+        return "Movistar", formatear_moneda(precio_movistar)
+
+    else:
+        return "Entrada", formatear_moneda(precio_estandar)
