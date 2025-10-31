a6e2220ef29d0606e9e6c79fe770a70f599c3842
Agustin Simon Padros <asimonpadros@uade.edu.ar>
2025-10-31 19:32:59 -0300

    Cambios

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Request, Query
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from app.data.peliculas import peliculas
+from app.logica.funciones import buscar_peliculas
 
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
